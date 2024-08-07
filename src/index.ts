ebbdd29d121bfb63b7b8598d169af8c53c39db7e
neytor <ortegajoseweb02@gmail.com>
2024-08-07 14:54:50 -0300

    updating cors again

--- src/index.ts
+++ src/index.ts
@@ -17,8 +17,9 @@ import swaggerUi from "swagger-ui-express"
 const app = express();
 const PORT = process.env.PORT || 3000;
 const corsOptions={
-  origin: 'http://localhost:5173/', 
+  origin: 'http://localhost:5173', 
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials:true
 }
 app.use(morgan("dev"))
 app.use(cors(corsOptions))
