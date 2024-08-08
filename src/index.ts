3800540665524d052aaad3d8712e132e7f9c4250
neytor <ortegajoseweb02@gmail.com>
2024-08-08 08:56:31 -0300

    fixing cookies error

--- src/index.ts
+++ src/index.ts
@@ -45,9 +45,9 @@ const {
   getSecret: () => "Secret", // A function that optionally takes the request and returns a secret
   cookieName: "__Host-psifi.x-csrf-token", // The name of the cookie to be used, recommend using Host prefix.
   cookieOptions: {
-    sameSite: "lax",
+    sameSite: 'none',
+    secure: true, 
     path: "/",
-    secure: true
   },
   size: 64, // The size of the generated tokens in bits
   ignoredMethods: ["GET", "HEAD", "OPTIONS"], // A list of request methods that will not be protected.
