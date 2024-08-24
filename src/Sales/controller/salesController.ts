a4afe1b0d0b11da997565348832471967400a403
jose <ortegajoseweb02@gmail.com>
2024-08-24 18:34:09 -0300

    refactor endpoints

--- src/Sales/controller/salesController.ts
+++ src/Sales/controller/salesController.ts
@@ -7,62 +7,62 @@ import mongoose from "mongoose";
 import { validateSchema } from "../../utils/utils.js";
 import { postSaleSchema } from "../zodSchema/salesZodSchema.js";
 
-export const postSale:RequestHandler = async (req,res,next) =>{
-    const {  saleItems } = req.body;
-    const ownerId = req.session.userId
-    const session = await mongoose.startSession();
-    session.startTransaction();
-    try {
-        validateSchema(postSaleSchema,{saleItems:saleItems,ownerId:ownerId})
-        if(saleItems.length <= 0){
-            throw createHttpError(400,"items is empty")
-        }
-        
-         // Process the sale
-         const totalAmount = await processSale(saleItems,session);
-
-         // Save the sale
-         const newSale = await saveSale(ownerId, saleItems, totalAmount,session);
-         await session.commitTransaction();
-        session.endSession();
-         res.status(201).json({ sale: newSale, totalAmount });
-    } catch (error) {
-        await session.abortTransaction();
-        session.endSession();
-        next(error)
-    }finally {
-        session.endSession();
+export const postSale: RequestHandler = async (req, res, next) => {
+  const { saleItems } = req.body;
+  const ownerId = req.session.userId
+  const session = await mongoose.startSession();
+  session.startTransaction();
+  try {
+    validateSchema(postSaleSchema, { saleItems: saleItems, ownerId: ownerId })
+    if (saleItems.length <= 0) {
+      throw createHttpError(400, "items is empty")
     }
-}
-interface getSaleBody{
-    userId:string
-}
-export const getSale:RequestHandler<unknown,unknown,getSaleBody,unknown> = async (req,res,next) =>{
-    const userId = req.session.userId
-    try {
 
-        const user = await verifyUser(userId)
-        const sale = await  UserTotalSales.findOne({owner:user._id}).exec()
-        if (!sale){
-            throw createHttpError(404,"Sales not found")
-        }
-        res.status(200).json(sale)
-    } catch (error) {
-        next(error)
+    // Process the sale
+    const totalAmount = await processSale(saleItems, session);
+
+    // Save the sale
+    const newSale = await saveSale(ownerId, saleItems, totalAmount, session);
+    await session.commitTransaction();
+    session.endSession();
+    res.status(201).json({ sale: newSale, totalAmount });
+  } catch (error) {
+    await session.abortTransaction();
+    session.endSession();
+    next(error)
+  } finally {
+    session.endSession();
+  }
+}
+interface getSaleBody {
+  userId: string
+}
+export const getSale: RequestHandler<unknown, unknown, getSaleBody, unknown> = async (req, res, next) => {
+  const userId = req.session.userId
+  try {
+
+    const user = await verifyUser(userId)
+    const sale = await UserTotalSales.findOne({ owner: user._id }).exec()
+    if (!sale) {
+      throw createHttpError(404, "Sales not found")
     }
+    res.status(200).json(sale)
+  } catch (error) {
+    next(error)
+  }
 }
 
-export const deleteSale:RequestHandler =async (req,res,next)=>{
-    const userId = req.session.userId
-    try {
-        const user =await verifyUser(userId)
-        const sale = await UserTotalSales.findOneAndDelete({owner:user._id})
-        console.log(sale)
-        if(!sale){
-            throw createHttpError(500,"something went wrong")
-        }
-        res.status(200).send("Sales deleted succesfully")
-    } catch (error) {
-        next(error)
+export const deleteSale: RequestHandler = async (req, res, next) => {
+  const userId = req.session.userId
+  try {
+    const user = await verifyUser(userId)
+    const sale = await UserTotalSales.findOneAndDelete({ owner: user._id })
+    console.log(sale)
+    if (!sale) {
+      throw createHttpError(500, "something went wrong")
     }
+    res.status(200).send({ message: "Sales deleted succesfully" })
+  } catch (error) {
+    next(error)
+  }
 }
