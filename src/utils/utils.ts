bbe0f40ca28a4163f9a32fdbc5ccbea50af3c335
jose <ortegajoseweb02@gmail.com>
2024-08-24 18:39:05 -0300

    fixing 500 error message

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,11 +1,11 @@
 import createHttpError from "http-errors";
 import { ZodSchema } from "zod";
 
-export const validateSchema = (schema:ZodSchema,data:object) =>{
-    const Validate = schema.safeParse(data)
-    if(!Validate.success){
-        const msg = Validate.error.errors.map((err) => {return err.message  } ).toString()
-        console.log(Validate.error.errors)
-        throw createHttpError(500,msg)
-    }
+export const validateSchema = (schema: ZodSchema, data: object) => {
+  const Validate = schema.safeParse(data)
+  if (!Validate.success) {
+    const msg = Validate.error.errors.map((err) => { return JSON.stringify(err) }).toString()
+    console.log(Validate.error.errors)
+    throw createHttpError(500, msg)
+  }
 } 
