bdbbb67d4fed4b0bd5a48cda7ebf03a54b51ab27
jose <ortegajoseweb02@gmail.com>
2024-08-26 18:05:58 -0300

    problems with logout

--- src/Users/controller/userController.ts
+++ src/Users/controller/userController.ts
@@ -1,4 +1,4 @@
-import {RequestHandler } from "express";
+import { RequestHandler } from "express";
 import bcrypt from "bcrypt";
 import userModel from "../models/userModel.js";
 import createHttpError from "http-errors";
@@ -8,97 +8,100 @@ import { validateSchema } from "../../utils/utils.js";
 
 
 export const getAuthenticatedUser: RequestHandler = async (req, res, next) => {
-    try {
-        const user = await userModel.findById(req.session.userId).select("+email").exec();
-        if(!user){
-            throw createHttpError(404,"User not found")
-        }
-        res.status(200).json(user);
-    } catch (error) {
-        next(error);
+  try {
+    const user = await userModel.findById(req.session.userId).select("+email").exec();
+    if (!user) {
+      throw createHttpError(404, "User not found")
     }
+    res.status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const logout: RequestHandler = (req, res, next) => {
-    req.session.destroy(error => {
-        if (error) {
-            next(error);
-        } else {
-            res.sendStatus(200);
-        }
-    });
+  if (!req.session || !req.session.userId) {
+    return res.status(400).json({ error: "user not log in" })
+  }
+  req.session.destroy(error => {
+    if (error) {
+      next(error);
+    } else {
+      res.sendStatus(200);
+    }
+  });
 };
 
-interface singUpBody{
-    username:String,
-    email:String,
-    password:String
+interface singUpBody {
+  username: String,
+  email: String,
+  password: String
 }
-export const signUp:RequestHandler<unknown,unknown,singUpBody,unknown> = async (req,res,next)=>{
-    const username = req.body.username
-    const email = req.body.email
-    const rawPassword:string = String(req.body.password)
-    try {
-        if(!username || !email || !rawPassword){
-            throw createHttpError(400,"Parameters missing")
-        }
-        validateSchema(registerSchema,req.body)
-        const existingUsername = await userModel.findOne({ username: username }).exec();
-
-        if (existingUsername) {
-            throw createHttpError(409, "Username already taken. Please choose a different one or log in instead.");
-        }
-
-        const existingEmail = await userModel.findOne({ email: email }).exec();
-
-        if (existingEmail) {
-            throw createHttpError(409, "A user with this email address already exists. Please log in instead.");
-        }
-        const hashedPassword:string = await bcrypt.hashSync(rawPassword,10)
-        const newUser = await userModel.create({
-            username: username,
-            email: email,
-            password: hashedPassword,
-            tables:[]
-        })
-        req.session.userId= newUser._id
-        res.status(201).json(newUser)
-    } catch (error) {
-        next(error)
+export const signUp: RequestHandler<unknown, unknown, singUpBody, unknown> = async (req, res, next) => {
+  const username = req.body.username
+  const email = req.body.email
+  const rawPassword: string = String(req.body.password)
+  try {
+    if (!username || !email || !rawPassword) {
+      throw createHttpError(400, "Parameters missing")
     }
+    validateSchema(registerSchema, req.body)
+    const existingUsername = await userModel.findOne({ username: username }).exec();
+
+    if (existingUsername) {
+      throw createHttpError(409, "Username already taken. Please choose a different one or log in instead.");
+    }
+
+    const existingEmail = await userModel.findOne({ email: email }).exec();
+
+    if (existingEmail) {
+      throw createHttpError(409, "A user with this email address already exists. Please log in instead.");
+    }
+    const hashedPassword: string = await bcrypt.hashSync(rawPassword, 10)
+    const newUser = await userModel.create({
+      username: username,
+      email: email,
+      password: hashedPassword,
+      tables: []
+    })
+    req.session.userId = newUser._id
+    res.status(201).json(newUser)
+  } catch (error) {
+    next(error)
+  }
 }
 
 
 
 
-interface loginBody{
-    username:string,
-    password:string
+interface loginBody {
+  username: string,
+  password: string
 }
 export const login: RequestHandler<unknown, unknown, loginBody, unknown> = async (req, res, next) => {
-    const username = req.body.username;
-    const password = req.body.password;
+  const username = req.body.username;
+  const password = req.body.password;
 
-    try {
-        if (!username || !password) {
-            throw createHttpError(400, "Parameters missing");
-        }
-        validateSchema(loginSchema,req.body)
-        const user = await userModel.findOne({ username: username }).select("+password +email").exec();
-
-        if (!user) {
-            throw createHttpError(401, "Invalid credentials");
-        }
-
-        const passwordMatch = await bcrypt.compare(password, user.password || "");
-
-        if (!passwordMatch) {
-            throw createHttpError(401, "Invalid credentials");
-        }
-
-        req.session.userId = user._id;
-        res.status(201).send({message:"Log in Succesfull,userId:" +user._id});
-    } catch (error) {
-        next(error);
+  try {
+    if (!username || !password) {
+      throw createHttpError(400, "Parameters missing");
     }
+    validateSchema(loginSchema, req.body)
+    const user = await userModel.findOne({ username: username }).select("+password +email").exec();
+
+    if (!user) {
+      throw createHttpError(401, "Invalid credentials");
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password || "");
+
+    if (!passwordMatch) {
+      throw createHttpError(401, "Invalid credentials");
+    }
+
+    req.session.userId = user._id;
+    res.status(201).send({ message: "Log in Succesfull,userId:" + user._id });
+  } catch (error) {
+    next(error);
+  }
 };
