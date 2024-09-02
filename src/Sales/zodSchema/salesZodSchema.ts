b0944fb28d332782d589723208643f42a808437b
jose <ortegajoseweb02@gmail.com>
2024-09-02 09:49:25 -0300

    fix bug sales

--- src/Sales/zodSchema/salesZodSchema.ts
+++ src/Sales/zodSchema/salesZodSchema.ts
@@ -1,12 +1,12 @@
 import { z } from "zod";
 const objectIdRegex = /^[a-fA-F0-9]{24}$/;
-const itemSchemaSale= z.object({
-    tableId: z.string().regex(objectIdRegex),
-    itemId: z.string().regex(objectIdRegex),
-    quantity: z.number().int().positive()
+const itemSchemaSale = z.object({
+  tableId: z.string().regex(objectIdRegex),
+  itemId: z.string().regex(objectIdRegex),
+  quantity: z.number().int().positive()
 })
 export const postSaleSchema = z.object({
-    salesItems: z.array(itemSchemaSale).nonempty({ message: "Items can't be empty!"}),
-    ownerId: z.string().regex(objectIdRegex)
+  saleItems: z.array(itemSchemaSale).nonempty({ message: "Items can't be empty!" }),
+  ownerId: z.string().regex(objectIdRegex)
 })
 
